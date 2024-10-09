267e431f2d9fa95a9ef33925c44e46a9c0c71bac
befi8957 <yamen.ajjour@uni-weimar.de>
2024-10-09 18:14:49 +0200

    ibmsc, ukp_sentential, semeval_2018

--- src/jobs/run-leave-out-experiment.sh
+++ src/jobs/run-leave-out-experiment.sh
@@ -6,6 +6,7 @@
 #SBATCH --time=24:00:00
 #SBATCH --output output/output.out
 #SBATCH --error errors/error.err
+#SBATCH --gpus=1
 conda activate lang
 #python /mnt/home/yajjour/task-specific-argument-mining-and-generation/src/experiment/process_jsonl.py -o /mnt/home/yajjour/task-specific-argument-mining-and-generation-data/
 python /mnt/home/yajjour/task-specific-argument-mining-and-generation/src/experiment/run.py -c /mnt/home/yajjour/task-specific-argument-mining-and-generation/src/experiment/configs/config_leave_one_out.json -c /mnt/home/yajjour/task-specific-argument-mining-and-generation/src/experiment/configs/config_leave_one_out_ajjour17.json
