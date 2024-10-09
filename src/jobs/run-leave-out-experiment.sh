57026d1b29466f337874ddd176a1bf430523a004
Yamen Ajjour <yajjour@svc4.kisski>
2024-10-09 15:51:32 +0200

    adding files

--- src/jobs/run-leave-out-experiment.sh
+++ src/jobs/run-leave-out-experiment.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -l
-#SBATCH --job-name=prompting-random
+#SBATCH --job-name=task-specific
 #SBATCH --nodes=1 
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=8G
 #SBATCH --time=24:00:00
 #SBATCH --output output/output.out
 #SBATCH --error errors/error.err
+module load Miniconda3
 conda activate lang
 python /mnt/home/yajjour/task-specific-argument-mining-and-generation/src/experiment/process_jsonl.py -o /mnt/home/yajjour/task-specific-argument-mining-and-generation-data/
 #python /mnt/home/yajjour/task-specific-argument-mining-and-generation/src/experiment/run.py
