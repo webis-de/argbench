80c44193137178cfa67e42199468bcdd539d3f88
Yamen Ajjour <yajjour@svc2.kisski>
2024-10-24 09:58:41 +0200

    adding config

--- src/jobs/run-leave-out-experiment.sh
+++ src/jobs/run-leave-out-experiment.sh
@@ -10,4 +10,4 @@
 module load Miniconda3
 conda activate lang
 #python /mnt/home/yajjour/task-specific-argument-mining-and-generation/src/experiment/process_jsonl.py -o /mnt/home/yajjour/task-specific-argument-mining-and-generation-data/
-python /mnt/home/yajjour/task-specific-argument-mining-and-generation/src/experiment/run.py -c /mnt/home/yajjour/task-specific-argument-mining-and-generation/src/experiment/configs/config_leave_one_out.json -c /mnt/home/yajjour/task-specific-argument-mining-and-generation/src/experiment/configs/config_leave_one_out_ajjour17.json
+python /mnt/home/yajjour/task-specific-argument-mining-and-generation/src/experiment/run.py -c /mnt/home/yajjour/task-specific-argument-mining-and-generation/src/experiment/configs/complete_leave_one_out_ajjour17.json -co output.json
