fc6ddd80ef3f0d27d9aa937265df341b667390cd
befi8957 <yamen.ajjour@uni-weimar.de>
2024-11-05 10:41:51 +0100

    ibmsc, ukp_sentential, semeval_2018

--- src/jobs/run-leave-out-experiment-luis.sh
+++ src/jobs/run-leave-out-experiment-luis.sh
@@ -10,4 +10,4 @@
 module load Miniconda3
 conda activate task-specific
 python /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/src/experiment/process_jsonl.py -o /mnt/home/yajjour/task-specific-argument-mining-and-generation-data/
-python /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/src/experiment/run.py -c /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/src/experiment/configs/config_leave_one_out.json -c /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/src/experiment/configs/config_leave_one_out_ajjour17.json
+python /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/src/experiment/run.py -c /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/src/experiment/configs/config_leave_one_out_ajjour17.json
