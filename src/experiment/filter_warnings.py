d32eca66d1322a8fb32a9b7970a41330b5b5fc1e
befi8957 <yamen.ajjour@uni-weimar.de>
2024-11-19 11:26:54 +0100

    leave-out for multiple tasks

--- src/experiment/filter_warnings.py
+++ src/experiment/filter_warnings.py
@@ -4,12 +4,12 @@ import warnings
 user_warnings_to_turn_off = [
     'TypedStorage is deprecated',
     'torch.utils.checkpoint',
-    'pad_token_id` should be positive but got -1',
-    'Input type into Linear4bit is torch.float16, but bnb_4bit_compute_dtype=torch.float32 (default). This will lead to slow inference or training speed',
+    '`pad_token_id` should be positive but got -1',
+    'Input type into Linear4bit is torch.float16',
     'Could not find a config file in /bigwork/nhwpajjy/pre-trained-models/baffo32/decapoda-research-llama-7B-hf'
 ]
 future_warnings_to_turn_off = [
-    "evaluation_strategy` is deprecate"
+    "`evaluation_strategy` is deprecate"
 ]
 def turn_off_warnings():
     for warning in user_warnings_to_turn_off:
