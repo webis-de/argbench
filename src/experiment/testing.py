6b09be142830b21aabe3675e4fe71c1fc8743938
befi8957 <yamen.ajjour@uni-weimar.de>
2024-11-21 10:36:23 +0100

    leave-out for multiple tasks

--- src/experiment/testing.py
+++ src/experiment/testing.py
@@ -176,6 +176,7 @@ def compute_sentence_f1(predictions, references, inputs):
         ground_truth_labels, reference_sentences = extract_sentence_labels(reference)
         logger.log(level=logging.INFO, msg=f"reference token size {len(reference.split())}")
         logger.log(level=logging.INFO, msg=f"prediction token size {len(prediction.split())}")
+        set_trace()
         for i, prediction_sentence in enumerate(prediction_sentences):
             if not (prediction_sentence == reference_sentences[i]):
                 logger.log(level=logging.ERROR, msg=f"model did not output all needed documents for evaluation predicted sentences count ")
