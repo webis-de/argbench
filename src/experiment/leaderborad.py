fc207c9282154826cbf62e379c0ca93d16270ce0
yamen <y.ajjour@ai.uni-hannover.de>
2024-12-03 14:55:24 +0100

    changing evaluation to include test df instead fo links

--- src/experiment/leaderborad.py
+++ src/experiment/leaderborad.py
@@ -1,10 +1,21 @@
+import os.path
+
 import pandas as pd
-class leaderboard:
+class Leaderboard:
 
     def __init__(self, output_path):
         self.output_path = output_path
 
 
     def read_file(self):
-        self.df_results = pd.read_csv(self.output_path)
+        if os.path.exists(self.output_path):
+            self.df_results = pd.read_csv(self.output_path)
+        else:
+            self.df_results = pd.DataFrame(columns=["model", "training_data", "task", "metric"])
 
+    def add_results(self, results):
+        df_record = pd.DataFrame(results)
+        self.df_results = pd.concat([self.df_results, df_record])
+
+    def save_file(self,results):
+        self.df_results.to_csv(self.output_path, sep="\t")
