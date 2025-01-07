6750c5a5bc754113e735e0f9f227427e4c22e3c0
yamen <y.ajjour@ai.uni-hannover.de>
2025-01-07 11:51:04 +0100

    fixing problems with different label sizKes

--- src/experiment/run.py
+++ src/experiment/run.py
@@ -602,7 +602,7 @@ class Runner:
             return compute_bleu_score(predictions, labels)
         elif metric == "meteor":
             return compute_meteor_score(predictions, labels)
-        elif metric == "["bio-fsc"]ore":
+        elif metric == "bio-fscore":
             return compute_bio_f1_score(predictions, labels, task_data["document"])
         elif metric == "sentence-fscore":
             return compute_sentence_f1(predictions, labels, task_data["document"])
