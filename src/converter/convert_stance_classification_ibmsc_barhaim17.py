3574523063e4823ada1f701c62690b7fe4515cae
yamen <y.ajjour@ai.uni-hannover.de>
2025-01-20 14:13:47 +0100

    droping seeds

--- src/converter/convert_stance_classification_ibmsc_barhaim17.py
+++ src/converter/convert_stance_classification_ibmsc_barhaim17.py
@@ -8,7 +8,8 @@ DATASET_NAME = "stance_classification_ibmsc_barhaim17"
 def process_dataset(dataset, path):
     output = Output(DATASET_NAME)
 
-    output.append_definition("Classify the stance of the following claim into Pro or Con. Output Pro if the claim supports the following topic and output Con if the claim attacks the following topic. Only answer with Con or Pro.")
+    output.append_definition("""Classify the stance of the following claim into Pro or Con. Answer with Pro if the following claim supports the following topic. Answer with Con if the claim attacks the topic.
+    Only answer with Pro or Con.""")
 
     for row in dataset.iterrows():
         row = row[1]
