543cbabe2bc7bfe521f5bb9d945b9da79d50f89d
yamen <y.ajjour@ai.uni-hannover.de>
2025-01-23 14:24:45 +0100

    running hpo on stance classification

--- src/experiment/run.py
+++ src/experiment/run.py
@@ -464,7 +464,7 @@ class Runner:
         metrics = self.evaluate(self.vllm, sampling_params, self.test_dataset, self.test_hf_dataset)
         log_mem(f"finished evaluation")
         logger.log(level=logging.INFO, msg=f"metrics are {metrics}")
-        self.free_vll_model()
+        self.free_vllm_model()
         log_mem(f"freed model")
         return metrics[self.config.hpo_config.val_metric]
 
