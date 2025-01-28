3c2e55ddc34d6c7b8eb38f968372504bd4862d71
NHWP25179 <y.ajjour@ai.uni-hannover.de>
2025-01-28 18:05:45 +0100

    finishing stuff

--- src/jobs/hpo-stance-barhaim17.sh
+++ src/jobs/hpo-stance-barhaim17.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
 #SBATCH --job-name=task-specific
 #SBATCH --nodes=1 
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=12
 #SBATCH --mem=8G
 #SBATCH --time=24:00:00
 #SBATCH --output /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/src/output/hpo-stance-barhaim.out
 #SBATCH --error /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/src/output/hpo-stance-barhaim.err
-#SBATCH --gpus=1
+#SBATCH --gpus=a100:1
 module load Miniconda3
 conda activate task-specific
 
