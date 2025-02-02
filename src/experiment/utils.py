df68375b1bebe875b7bdaa77ff2e2f41ae9c8513
yamen <y.ajjour@ai.uni-hannover.de>
2025-02-02 19:35:31 +0100

    fixing logging and renaming files.

--- src/experiment/utils.py
+++ src/experiment/utils.py
@@ -8,7 +8,4 @@ def get_logger(name):
     file_handler.setFormatter(logging.Formatter("%(asctime)s %(message)s)"))
     logging.getLogger(name).addHandler(file_handler)
     logging.getLogger(name).setLevel(logging.DEBUG)
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.DEBUG)
-    logging.getLogger(name).addHandler(stream_handler)
     return logging.getLogger(name)
