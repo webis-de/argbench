67ef42fbf63835d745a92eee2b7e03d0fab1a001
yamen <y.ajjour@ai.uni-hannover.de>
2025-02-02 20:02:13 +0100

    fixing logging and renaming files.

--- src/experiment/utils.py
+++ src/experiment/utils.py
@@ -3,9 +3,10 @@ from config import *
 
 def get_logger(name):
     logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
     file_handler = logging.FileHandler(f"/bigwork/nhwpajjy/task-specific-argument-mining-and-generation-data/logs/{name}-log.log")
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(logging.Formatter("%(asctime)s %(message)s)"))
     logger.addHandler(file_handler)
-    logger.setLevel(logging.DEBUG)
+
     return logger
