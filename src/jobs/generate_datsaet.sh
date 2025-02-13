e16f6c231eda74ad8cbe0de7f6cccbeeaf3b0824
yamen <y.ajjour@ai.uni-hannover.de>
2025-02-13 23:52:14 +0100

    cleaned and renamed files

--- src/jobs/generate_datsaet.sh
+++ src/jobs/generate_datsaet.sh
@@ -1,4 +1,15 @@
 #!/bin/bash -l
+#SBATCH --job-name=preprocessing-datasets
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=8G
+#SBATCH --time=24:00:00
+#SBATCH --output /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/src/output/preprocessing-datasets.out
+#SBATCH --error /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/src/output/preprocessing-datasets.err
 
-python "../converter/convert_$1.py"
-python "../experiment/preprocess.py" -t "$1" -o /bigwork/nhwpajjy/task-specific-argument-mining-and-generation-data/tasks
+module load Miniforge3
+conda activate task-specific
+for f in bigwork/nhwpajjy/task-specific-argument-mining-and-generation/src/converter/convert*.py;  do
+python $f ;
+echo $f
+done
