8b973075f358e1b74dfc9b7587f1372432bdf3e3
yamen <y.ajjour@ai.uni-hannover.de>
2025-02-21 13:19:40 +0100

    adding metrics

--- src/experiment/testing.py
+++ src/experiment/testing.py
@@ -171,7 +171,6 @@ def convert_to_bio(input, output, label_mappings):
 
 
 def compute_bio_f1_score(predictions, references, inputs, label_mappings):
-    set_trace()
     all_labels = [ ]
     all_predictions = []
     #set_trace()
