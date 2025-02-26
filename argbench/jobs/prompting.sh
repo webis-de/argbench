dc04e02e5c9c73930849247d8d4f3c34dff1ec12
yamen <y.ajjour@ai.uni-hannover.de>
2025-02-26 21:19:25 +0100

    -adding bleu score - fixing missing datasets problems

--- argbench/jobs/prompting.sh
+++ argbench/jobs/prompting.sh
@@ -4,10 +4,10 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=15G
 #SBATCH --time=24:00:00
-#SBATCH --output /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/argbench/output/prompting-mistral.out
-#SBATCH --error /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/argbench/output/prompting-mistral.err
+#SBATCH --output /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/argbench/output/prompting-gemma.out
+#SBATCH --error /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/argbench/output/prompting-gemma.err
 #SBATCH --gpus=a100:1
 module load Miniforge3
 conda activate task-specific
 cd /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/
-python -m  argbench.experiment.run -c /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/argbench/experiment/configs/prompting/mistral_prompting.json
+python -m  argbench.experiment.run -c /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/argbench/experiment/configs/prompting/gemma_prompting.json
