42be578c5ac06cc0b3bbd5859ec113e71fcda01c
yamen <y.ajjour@ai.uni-hannover.de>
2025-03-12 12:52:01 +0100

    -adding bleu score - fixing missing datasets problems

--- argbench/jobs/generate_datsaet.sh
+++ argbench/jobs/generate_datsaet.sh
@@ -1,2 +1,2 @@
-python "../converter/convert_$1.py"
-python "../experiment/preprocess.py" -t "$1" -o /bigwork/nhwpajjy/task-specific-argument-mining-and-generation-data/tasks
+python "argbench/converter/convert_$1.py"
+python -m argbench.experiment.preprocess -t "$1" -o /bigwork/nhwpajjy/task-specific-argument-mining-and-generation-data/tasks
