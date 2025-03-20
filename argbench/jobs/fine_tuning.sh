9a2b4ce30a2119307942c51c15090cb6d30416b9
Yamen <y.ajjour@ai.uni-hannover.de>
2025-03-20 10:28:44 +0100

    fine tuning on stance barhaim

--- argbench/jobs/fine_tuning.sh
+++ argbench/jobs/fine_tuning.sh
@@ -4,13 +4,13 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=15G
 #SBATCH --time=24:00:00
-#SBATCH --output /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/argbench/output/prompting.out
-#SBATCH --error /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/argbench/output/prompting.err
+#SBATCH --output /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/argbench/output/fine-tuning.out
+#SBATCH --error /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/argbench/output/fine-tuning.err
 #SBATCH --gpus=a100:1
 module load Miniforge3
 conda activate task-specific
 cd /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/
 model=$1
-python -m  argbench.experiment.run -c /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/argbench/experiment/configs/prompting/fine_tuning \
+python -m  argbench.experiment.run -c /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/argbench/experiment/configs/prompting/fine_tuning.json \
 --leaderboard-path "/bigwork/nhwpajjy/task-specific-argument-mining-and-generation-data/runs/prompting-$model-results.csv" \
 --base_model "$model" --test_dataset "stance_classification_ibmsc_barhaim17"
