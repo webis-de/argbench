cc22c72b217da72bd401c2db51da4dec1aff1f25
Yamen <y.ajjour@ai.uni-hannover.de>
2025-03-21 12:02:56 +0100

    fine tuning on stance barhaim

--- argbench/jobs/fine_tuning.sh
+++ argbench/jobs/fine_tuning.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
-#SBATCH --job-name=fine-tuning
+#SBATCH --job-name=fine-tuning-2
 #SBATCH --nodes=1 
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=15G
 #SBATCH --time=24:00:00
-#SBATCH --output /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/argbench/output/fine-tuning.out
-#SBATCH --error /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/argbench/output/fine-tuning.err
+#SBATCH --output /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/argbench/output/fine-tuning-2.out
+#SBATCH --error /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/argbench/output/fine-tuning-2.err
 #SBATCH --gpus=a100:1
 module load Miniforge3
 conda activate task-specific
@@ -14,6 +14,7 @@ model=$1
 dataset=$2
 echo $model
 echo $dataset
-python -m  argbench.experiment.run -c /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/argbench/experiment/configs/instruction-fine-tuning/fine_tuning.json \
---leaderboard-path "/bigwork/nhwpajjy/task-specific-argument-mining-and-generation-data/runs/fine-tuning-$model-results.csv" \
---base_model "$model" --test_dataset_name "$dataset"
+#python -m  argbench.experiment.run -c /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/argbench/experiment/configs/instruction-fine-tuning/fine_tuning.json \
+#--leaderboard-path "/bigwork/nhwpajjy/task-specific-argument-mining-and-generation-data/runs/fine-tuning-$model-results.csv" \
+#--base_model "$model" --test_dataset_name "$dataset"
+python -c "1*2"
