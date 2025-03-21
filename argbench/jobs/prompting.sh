89573dc521751d7788848a69f1636569e3ce3d27
Yamen <y.ajjour@ai.uni-hannover.de>
2025-03-21 12:07:21 +0100

    fine tuning on stance barhaim

--- argbench/jobs/prompting.sh
+++ argbench/jobs/prompting.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH --job-name=prompting
 #SBATCH --nodes=1 
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=12
 #SBATCH --mem=15G
 #SBATCH --time=24:00:00
 #SBATCH --output /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/argbench/output/prompting.out
