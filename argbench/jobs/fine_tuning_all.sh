fc5b422b0efe06e9883702346d939f93d16fa6e9
Yamen <y.ajjour@ai.uni-hannover.de>
2025-03-24 22:51:05 +0100

    fine tuning on stance barhaim

--- argbench/jobs/fine_tuning_all.sh
+++ argbench/jobs/fine_tuning_all.sh
@@ -10,12 +10,11 @@
 module load Miniforge3
 conda activate task-specific
 cd /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/
-model=$1
-dataset=$2
-echo $model
-echo $dataset
+dataset=$1
 
-python -m  argbench.experiment.run -c /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/argbench/experiment/configs/instruction-fine-tuning/fine_tuning.json \
---leaderboard-path "/bigwork/nhwpajjy/task-specific-argument-mining-and-generation-data/runs/fine-tuning-$model-results.csv" \
---base_model "$model" --test_dataset_name "$dataset"
-python -c "print(1*2)"
+for model in mistral-7b-inst-3 gemma-2-9b-it mistral-small llama-3-8b-instruct deepseek-r1-distill-1.5b qwen-7b ;
+do
+  python -m  argbench.experiment.run -c /bigwork/nhwpajjy/task-specific-argument-mining-and-generation/argbench/experiment/configs/instruction-fine-tuning/fine_tuning.json \
+  --leaderboard-path "/bigwork/nhwpajjy/task-specific-argument-mining-and-generation-data/runs/fine-tuning-$model-results.csv" \
+  --base_model "$model" --test_dataset_name "$dataset"
+done
