df9301fca0fd9fab1f333b5a4b544ce211b6a360
Yamen <y.ajjour@ai.uni-hannover.de>
2025-03-25 17:36:14 +0100

    migrating to kisski

--- argbench/jobs/preprocess_all_datsaets.sh
+++ argbench/jobs/preprocess_all_datsaets.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=preprocess-all-dataset
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=25G
+#SBATCH --mem=50G
 #SBATCH --time=24:00:00
 #SBATCH --output argbench/output/preprocess-all-dataset.out
 #SBATCH --error argbench/output/preprocess-all-dataset.err
