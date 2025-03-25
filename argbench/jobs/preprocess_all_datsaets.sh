8e71a13e023ba7fb8737bdaf9aabdbd26ff8a3d8
Yamen <y.ajjour@ai.uni-hannover.de>
2025-03-25 17:10:53 +0100

    migrating to kisski

--- argbench/jobs/preprocess_all_datsaets.sh
+++ argbench/jobs/preprocess_all_datsaets.sh
@@ -14,7 +14,7 @@ export DATA_PATH="$BIGWORK/task-specific-argument-mining-and-generation-data"
 module load Miniforge3
 conda activate task-specific
 
-for f in "$CODA_PATH/argbench/converter/convert*.py";  do
+for f in "${CODA_PATH}/argbench/converter/convert*.py";  do
 python $f ;
 echo $f
 done
