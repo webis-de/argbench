f22b12de95b1d824e36b33cbfcb6ff877a83b6bc
Yamen <y.ajjour@ai.uni-hannover.de>
2025-03-27 07:11:35 +0100

    migrating to kisski

---
+++ argbench/experiment/tests/test_config_rewrite.py
@@ -0,0 +1,7 @@
+import unittest
+from argbench.experiment.utils import *
+
+class testConfigRewrite(unittest.TestCase):
+
+    def test_config_rewrite(self):
+        adjust_config("/bigwork/nhwpajjy")
