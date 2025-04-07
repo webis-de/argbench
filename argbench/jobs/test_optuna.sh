edb54aec582bde46f41311338ffea31c88a38a17
Yamen <y.ajjour@ai.uni-hannover.de>
2025-04-07 11:11:47 +0200

    migrating to kisski

---
+++ argbench/jobs/test_optuna.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+#SBATCH --job-name=prmt-all-data
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=25G
+#SBATCH --time=24:00:00
+#SBATCH --output argbench/output/prmt-all-data.out
+#SBATCH --error argbench/output/prmt-all-data.err
+#SBATCH --gpus=1
+module load Miniforge3
+conda activate task-specific
+
+model=$1
+
+export CODE_PATH="$BIGWORK/task-specific-argument-mining-and-generation"
+export DATA_PATH="$BIGWORK/task-specific-argument-mining-and-generation-data"
+
+cd "$CODE_PATH"
+
+python -c "import optuna"
