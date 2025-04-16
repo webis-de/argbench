3f3c3b18ef9e6aaf78fb9f0ee2db001265a09cd7
Yamen <y.ajjour@ai.uni-hannover.de>
2025-04-16 11:11:52 +0200

    adding filter to results

--- argbench/experiment/leaderborad.py
+++ argbench/experiment/leaderborad.py
@@ -28,7 +28,10 @@ class Leaderboard:
     def add_aggregated_results(self):
         metadata = get_metadata()
         task_skill_mapping = {task: metadata[task]["skill"] for task in metadata}
-
+        generation_blue_results = 0
+        generation_bertscore_results = 0
+        generation_task_count = 0
+        ### Skill results contain results for all tasks except generation
         skill_results = {skill: 0 for skill in task_skill_mapping.values()}
         skill_counts = {skill: 0 for skill in task_skill_mapping.values()}
         skill_records = {}
@@ -37,10 +40,17 @@ class Leaderboard:
         for task_result in self.added_results:
             test_task = task_result["test_task"]
             skill = task_skill_mapping[test_task]
-            if task_result["metric"] == "fscore" or task_result["metric"]== "bertscore":
-                skill_results[skill] += task_result["score"]
-                skill_counts[skill] += 1
-                skill_records[skill]={}
+            if skill == "generation":
+                if task_result["metric"] == "bertscore":
+                    generation_bertscore_results += task_result["score"]
+                elif task_result["metric"] == "bleu":
+                    generation_blue_results += task_result["score"]
+                generation_task_count += 1
+            else:
+                if task_result["metric"] == "fscore":
+                    skill_results[skill] += task_result["score"]
+                    skill_counts[skill] += 1
+                    skill_records[skill]={}
 
         if self.added_results:
             model = self.added_results[0]["model"]
@@ -48,15 +58,21 @@ class Leaderboard:
             k = self.added_results[0]["k"]
             filter = self.added_results[0]["filter"]
 
+
         for skill in skill_records:
             score = skill_results[skill] / skill_counts[skill]
-            if skill == "generation":
-                metric = "bertscore"
-            else:
-                metric =  "fscore"
+            metric =  "fscore"
             skill_records[skill] = {"model": model, "start_time": start_time, "k": k, "score": score, "metric": metric, "test_task": skill, "filter" : filter}
             df_record = pd.DataFrame([skill_records[skill]])
             self.df_results = pd.concat([self.df_results, df_record])
+
+        if generation_task_count:
+            aggregated_bleu_results = generation_blue_results/generation_task_count
+            aggregtged_bertscore_results = generation_bertscore_results/generation_task_count
+            bleu_record = pd.DataFrame([{"model": model, "start_time": start_time, "k": k, "score": aggregated_bleu_results, "metric": "bleu", "test_task": "generation", "filter" : filter}])
+            bertscore_record =  pd.DataFrame([{"model": model, "start_time": start_time, "k": k, "score": aggregtged_bertscore_results, "metric": "bertscore", "test_task": "generation", "filter" : filter}])
+            self.df_results = pd.concat([self.df_results, bleu_record])
+            self.df_results = pd.concat([self.df_results, bertscore_record])
 
     def add_results(self, results):
         """
