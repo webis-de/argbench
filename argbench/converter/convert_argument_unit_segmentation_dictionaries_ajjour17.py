d87eadfd85e3d3f1cd0b7a1233a636d5a1494204
Yamen <y.ajjour@ai.uni-hannover.de>
2025-05-01 12:45:28 +0200

    - retrieving back non-dictionary segmentation tasks - separating dictionaries and non-dictionaries tasks

---
+++ argbench/converter/convert_argument_unit_segmentation_dictionaries_ajjour17.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python3
+from pathlib import Path
+
+from common import split_val_train
+from common import Genres, Output, Skills, datasets_path, tasks_path, Metadata, add_seed_arg, set_seed
+from dataclasses import dataclass
+from argparse import ArgumentParser
+from typing import List
+from tqdm import tqdm
+import random
+
+
+
+@dataclass
+class Unit:
+    span: str
+    label: str
+
+@dataclass
+class Units:
+    fragment_id: str
+    units: List #( span, argumentative or non-argumentative)
+    
+    text: str
+
+def extract_file(path: Path):
+    datafile = open(path, "r")
+    units = Units(path.name, [], "")
+
+    is_next_skip = False
+    idx = -1
+    last_token_label = None
+    non_arg_start = 0
+    is_inside_non_arg = False
+    for i, line in tqdm(enumerate(datafile)):
+
+        token_row = line.split("\t\t")
+
+        if token_row[5] == "POS" and token_row[1] == "'":
+            token = token_row[1]
+            is_next_skip = True
+        elif token_row[5] == "(":
+            token = " " + token_row[1]
+            is_next_skip = True
+        elif (token_row[5] == "SENT"
+              or token_row[5] == ","
+              or token_row[5] == "''"
+              or token_row[5] == ")"
+              or token_row[5] == ":"):
+            token = token_row[1]
+        elif is_next_skip:
+            token = token_row[1]
+            is_next_skip = False
+        else:
+            token = " " + token_row[1]
+
+        if token_row[0] == "Arg-B":
+            if is_inside_non_arg:
+
+                is_inside_non_arg = False
+            idx += 1
+            units.units.append(Unit("", "Argumentative"))
+            units.units[idx].span += token
+
+        elif token_row[0] == "Arg-I":
+            units.units[idx].span += token
+
+        elif token_row[0] == "Arg-O" and (last_token_label == "Arg-I" or i == 0):
+            is_inside_non_arg = True
+            idx += 1
+            units.units.append(Unit("", "Non-argumentative"))
+            units.units[idx].span += token
+
+        elif token_row[0] == "Arg-O" and is_inside_non_arg:
+            units.units[idx].span += token
+
+
+        units.text += token
+        last_token_label = token_row[0]
+
+    units.text.strip()
+
+    datafile.close()
+    return units
+
+DATASET_NAME_TEMPLATE = "argument_unit_segmentation_dictionaries_{dataset}_ajjour17"
+DATASET_FILE_TEMPLATE = "argument_unit_segmentation_dictionaries_{dataset}_{split}_ajjour17.json"
+
+def process_folder(path: Path):
+    train_dataset = []
+    test_dataset = []
+    val_dataset = []
+    train_path = path / "trainingSet"
+    test_path = path / "testingSet"
+
+    train_files = list(train_path.iterdir())
+    val_files, train_files = split_val_train(train_files)
+
+    for f in train_files:
+        text = extract_file(f)
+        train_dataset.append(text)
+
+    for f in val_files:
+        text = extract_file(f)
+        val_dataset.append(text)
+
+    for f in test_path.iterdir():
+        text = extract_file(f)
+        test_dataset.append(text)
+
+
+    return test_dataset, val_dataset, train_dataset
+
+
+def convert_arguments(dataset_name, test_dataset, val_dataset, train_dataset):
+    prompt = """Given the following document, split all of the document into argumentative units and non-argumentative units.
+An argumentative unit is a statement that has an argumentative function for example a claim or anecdote.
+Prepend each argumentative unit with argumentative: and spans that are not Argumentative with Non-argumentative:.
+Output the extracted spans as a list of dictionaries with the spans as keys and Argumentative or Non-argumentative as values.
+Do not add a new formating or enumeration also do not rephrase the argument units. Order the output spans as they appear in the document."""
+
+    train_output = Output(dataset_name)
+    test_output = Output(dataset_name)
+    val_output = Output(dataset_name)
+
+    train_output.append_definition(prompt)
+    test_output.append_definition(prompt)
+    val_output.append_definition(prompt)
+
+    for file in tqdm(train_dataset):
+        extracted_units = [{unit.span.strip():unit.label} for unit in file.units]
+        train_output.append_instance(file.fragment_id, file.text, [extracted_units])
+
+    for file in tqdm(test_dataset):
+        extracted_units = [{unit.span.strip():unit.label} for unit in file.units]
+        test_output.append_instance(file.fragment_id, file.text, [extracted_units])
+
+    for file in tqdm(val_dataset):
+        extracted_units = [{unit.span.strip():unit.label} for unit in file.units]
+        val_output.append_instance(file.fragment_id, file.text, [extracted_units])
+
+    return test_output, val_output, train_output
+
+
+def find_matching_non_arg(non_arg_seqs, bin_start, bin_end):
+
+    matching_spans = []
+    for seqs_start, seqs_end in non_arg_seqs:
+        span_len = seqs_end - seqs_start
+        if span_len >= bin_start:
+            matching_spans.append((seqs_start, seqs_end, span_len))
+
+    random.shuffle(matching_spans)
+    matched_span = matching_spans[0]
+
+    span_offset = int(matched_span[2] - bin_end)
+
+    if span_offset <= 0:
+        return (matched_span[0], matched_span[1])
+
+    offset = random.randrange(span_offset)
+
+    if random.random() < 0.5:
+        return (matched_span[0] + offset, matched_span[1])
+    return (matched_span[0], matched_span[1] - offset)
+
+
+if __name__ == "__main__":
+
+    arg_parser = ArgumentParser(description="Program to convert ajjour unit segmentation dataset into appropriate form")
+    add_seed_arg(arg_parser)
+    args = arg_parser.parse_known_args()[0]
+    set_seed(args)
+
+
+    output_path = tasks_path()
+    for dataset in ["editorials", "essays", "webDiscourse"]:
+        dataset_path = (datasets_path() / "unit-segmentation" / "simple" / dataset)
+
+        dataset_name = DATASET_NAME_TEMPLATE.replace("{dataset}", dataset)
+        dataset_file_train = DATASET_FILE_TEMPLATE.replace("{dataset}", dataset).format(split="train")
+        dataset_file_test = DATASET_FILE_TEMPLATE.replace("{dataset}", dataset).format(split="test")
+        dataset_file_val = DATASET_FILE_TEMPLATE.replace("{dataset}", dataset).format(split="val")
+
+        test_data, val_data, train_data  = process_folder(dataset_path)
+        test_data, val_data, train_data = convert_arguments(dataset_name, test_data, val_data, train_data)
+
+        if dataset == "essays":
+            genre = Genres.ESSAYS
+        elif dataset == "editorials":
+            genre = Genres.NEWS
+        else:
+            genre = Genres.WEB_FORUMS
+        train_data.append_genre(genre)
+        test_data.append_genre(genre)
+
+        train_data.append_subarea(Skills.MINING)
+
+        train_data.write_output(dataset_file_train)
+        test_data.write_output(dataset_file_test)
+        val_data.write_output(dataset_file_val)
+
+        metadata = Metadata(dataset_name)
+        metadata.add_dataset(dataset_file_train, "train")
+        metadata.add_dataset(dataset_file_test, "test")
+        metadata.add_dataset(dataset_file_val, "val")
+
+        metadata.add_genre(genre)
+        metadata.add_evaluation_metric("argument-dict-fscore")
+        metadata.add_skill(Skills.MINING)
+        metadata.write_metadata()
