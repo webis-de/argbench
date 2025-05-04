102d1a584d077fd5ca439c0ee04ec526371f082c
Yamen <y.ajjour@ai.uni-hannover.de>
2025-05-04 20:49:39 +0200

    - implemented few shot template

--- argbench/jobs/run_in_tasks_experiments.sh
+++ argbench/jobs/run_in_tasks_experiments.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=96:00:00
 #SBATCH --output argbench/output/in-task-hpo.out
 #SBATCH --error argbench/output/in-task-hpo.err
-#SBATCH --gpus=a100:1
+#SBATCH --gpus=a100:2
 module load Miniforge3
 conda activate task-specific
 
