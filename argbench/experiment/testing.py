114bf0478c6e25089af97be0f3a42268eb525379
Yamen <y.ajjour@ai.uni-hannover.de>
2025-05-06 18:13:19 +0200

    - fixing segmentation bugs

--- argbench/experiment/testing.py
+++ argbench/experiment/testing.py
@@ -114,8 +114,11 @@ def compute_bleu_score(predictions, references):
 
     references = [[word_tokenize(ref)] for ref in references]
     predictions = [word_tokenize(pred) for pred in predictions]
-    bleu = nltk.translate.bleu_score.corpus_bleu(references, predictions, weights=(0.5, 0.5))
-
+    try:
+        bleu = nltk.translate.bleu_score.corpus_bleu(references, predictions, weights=(0.5, 0.5))
+    except:
+        bleu = 0
+        logger.debug(f"Problem with calculating bleu ref {references} and pred {predictions}")
     return {"bleu": bleu}
 
 def compute_bert_score(predictions, references):
--- argbench/experiment/tests/test_bleuscore.py
+++ argbench/experiment/tests/test_bleuscore.py
@@ -2,7 +2,7 @@ from argbench.experiment.testing  import *
 from unittest import TestCase
 
 class BleuScoreTest(TestCase):
-    def testBleuScore(self):
+    def testBleuScore1(self):
         score = compute_bleu_score(['it is a white cat .',
                             'wow , this dog is huge .'], ['it is a white kitten .',
                                                           'wowww , the dog is huge !'])
@@ -10,8 +10,20 @@ class BleuScoreTest(TestCase):
         print(score)
 
 
-    def testBleuScore(self):
+    def testBleuScore2(self):
         score = compute_bleu_score(['We should ban plastic bags since they harm wildlife and contribute to pollution'],
         ['Plastic bags should be banned because they are harmful to wildlife and contribute to environmental pollution'])
+        self.assertTrue(score)
+        print(score)
+
+    def testBleuScore3(self):
+        score = compute_bleu_score([],
+                                   [])
+        self.assertTrue(score)
+        print(score)
+
+    def testBleuScore4(self):
+        score = compute_bleu_score([""],
+                                   [""])
         self.assertTrue(score)
         print(score)
