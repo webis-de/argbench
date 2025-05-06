278e2921bc8bba830f8afe8642ef74f9f3d829dd
Yamen <y.ajjour@ai.uni-hannover.de>
2025-05-06 18:07:56 +0200

    - fixing segmentation bugs

--- argbench/experiment/testing.py
+++ argbench/experiment/testing.py
@@ -112,7 +112,7 @@ def compute_bleu_score(predictions, references):
 
 
 
-    references = [word_tokenize(ref) for ref in references]
+    references = [[word_tokenize(ref)] for ref in references]
     predictions = [word_tokenize(pred) for pred in predictions]
     bleu = nltk.translate.bleu_score.corpus_bleu(references, predictions, weights=(0.5, 0.5))
 
--- argbench/experiment/tests/test_bleuscore.py
+++ argbench/experiment/tests/test_bleuscore.py
@@ -8,3 +8,10 @@ class BleuScoreTest(TestCase):
                                                           'wowww , the dog is huge !'])
         self.assertTrue(score)
         print(score)
+
+
+    def testBleuScore(self):
+        score = compute_bleu_score(['We should ban plastic bags since they harm wildlife and contribute to pollution'],
+        ['Plastic bags should be banned because they are harmful to wildlife and contribute to environmental pollution'])
+        self.assertTrue(score)
+        print(score)
