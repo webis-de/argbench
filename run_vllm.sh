a4eccefa77a516a0980f59d27a6f5cad1a2cecd9
Yamen <y.ajjour@ai.uni-hannover.de>
2025-05-06 23:27:33 +0200

    - fixing segmentation bugs

--- run_vllm.sh
+++ run_vllm.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=25G
-#SBATCH --time=96:00:00
+#SBATCH --time=12:00:00
 #SBATCH --output argbench/output/run_vllm.out
 #SBATCH --error argbench/output/run_vllm.err
 #SBATCH --gpus=a100:1
