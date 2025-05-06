c6a82ee62875f69826a7ee67b0b61caeb65175a5
Yamen <y.ajjour@ai.uni-hannover.de>
2025-05-06 10:00:46 +0200

    - fixing segmentation bugs

---
+++ prompting_parallel.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -l
+#SBATCH --job-name=prmt-all-data-parallel
+#SBATCH --nodes=1 
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=25G
+#SBATCH --time=96:00:00
+#SBATCH --output argbench/output/prmt-all-data-parallel.out
+#SBATCH --error argbench/output/prmt-all-data-parallel.err
+#SBATCH --gpus=a100:2
+module load Miniforge3
+conda activate task-specific
+
+model=$1
+
+export CODE_PATH="$BIGWORK/task-specific-argument-mining-and-generation"
+export DATA_PATH="$BIGWORK/task-specific-argument-mining-and-generation-data"
+
+cd "$CODE_PATH"
+
+if [ -z "$model" ]; then
+
+models=$(jq '.model_configs[].label' "${CODE_PATH}/argbench/experiment/configs/prompting/prompting.json")
+
+else
+
+models=($model)
+fi
+
+for model in ${models[@]};
+do
+  echo "$model"
+python -m  argbench.experiment.run -c "${CODE_PATH}/argbench/experiment/configs/prompting/prompting.json" \
+--leaderboard-path "${DATA_PATH}/runs/prompting-$model-results.csv" --base_model "$model" --debug
+done
+
+export TIME="$(sacct --format=Elapsed -j $SLURM_JOB_ID | tail -n 1 | xargs 2>&1)"
+export JOB_ARGUMENTS="prompting;all-datasets;${model};\n"
+echo "$SLURM_JOB_ID,$SLURM_JOB_NAME,$TIME,$JOB_ARGUMENTS" >> "$CODE_PATH/argbench/jobs/job-accounting.csv"
