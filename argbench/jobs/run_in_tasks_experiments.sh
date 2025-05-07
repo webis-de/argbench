e545292abeff1a38f9e48d9272985855c9d18a93
Yamen <y.ajjour@ai.uni-hannover.de>
2025-05-07 22:29:45 +0200

    tensor-parallel=2

--- argbench/jobs/run_in_tasks_experiments.sh
+++ argbench/jobs/run_in_tasks_experiments.sh
@@ -4,7 +4,9 @@
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=20G
 #SBATCH --time=96:00:00
-#SBATCH --gpus=a100:1
+#SBATCH --output argbench/output/in-task-hpo-%j.out
+#SBATCH --error argbench/output/in-task-hpo-%j.err
+#SBATCH --gpus=a100:2
 module load Miniforge3
 conda activate task-specific
 
