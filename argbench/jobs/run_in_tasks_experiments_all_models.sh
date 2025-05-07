4135ddae310ebdcf6c4ff7fc38c1e389d632f710
Yamen <y.ajjour@ai.uni-hannover.de>
2025-05-07 21:58:26 +0200

    tensor-parallel=2

--- argbench/jobs/run_in_tasks_experiments_all_models.sh
+++ argbench/jobs/run_in_tasks_experiments_all_models.sh
@@ -4,5 +4,5 @@ models=("gemma-9-2b-it" "gemma-2-27b-it")
 
 for model in ${models[@]};
 do
-sbatch argbench/jobs/run_in_tasks_experiments.sh "$model"
+sbatch argbench/jobs/run_in_tasks_experiments.sh "$model" --output "argbench/output/in-task-experiment-$model.out"
 done
