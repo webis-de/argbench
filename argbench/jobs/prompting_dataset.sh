1415491db8a16178dfa62f29be6f07cd4f138214
Yamen <y.ajjour@ai.uni-hannover.de>
2025-05-07 21:05:10 +0200

    tensor-parallel=2

--- argbench/jobs/prompting_dataset.sh
+++ argbench/jobs/prompting_dataset.sh
@@ -12,16 +12,30 @@
 module load Miniforge3
 conda activate task-specific
 
-model=$1
-dataset=$2
+model=$2
+dataset=$1
+
 
 export CODE_PATH="$BIGWORK/task-specific-argument-mining-and-generation"
 export DATA_PATH="$BIGWORK/task-specific-argument-mining-and-generation-data"
 
+if [ -z "$model" ]; then
+
+#models=$(jq '.model_configs[].label' "${CODE_PATH}/argbench/experiment/configs/prompting/prompting.json")
+models=("gemma-9-2b-it" "gemma-2-27b-it" "mistral-7b-inst-3" "mistral-small" "deepseek-r1-distill-7b" "deepseek-r1-distill-32b")
+else
+
+models=($model)
+fi
+
 cd "$CODE_PATH"
+
+for model in ${models[@]};
+do
 
 python -m  argbench.experiment.run -c "${CODE_PATH}/argbench/experiment/configs/prompting/prompting.json" \
 --leaderboard-path "${DATA_PATH}/runs/prompting-$model-results.csv" --base_model "$model" --test_dataset_name "$dataset" --debug
+done
 
 export TIME="$(sacct --format=Elapsed -j $SLURM_JOB_ID | tail -n 1 | xargs 2>&1)"
 export JOB_ARGUMENTS="prompting;${dataset};${model};\n"
