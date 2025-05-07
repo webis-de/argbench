8c8dfb305893aa75d095f1e0189057401f7b6c08
Yamen <y.ajjour@ai.uni-hannover.de>
2025-05-07 21:44:49 +0200

    tensor-parallel=2

--- argbench/jobs/prompting_dataset.sh
+++ argbench/jobs/prompting_dataset.sh
@@ -4,8 +4,8 @@
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=25G
 #SBATCH --time=96:00:00
-#SBATCH --output argbench/output/prmt-dataset.out
-#SBATCH --error argbench/output/prmt-dataset.err
+#SBATCH --output argbench/output/prmt-$1.out
+#SBATCH --error argbench/output/prmt-$1.err
 #SBATCH --gpus=a100:1
 
 
