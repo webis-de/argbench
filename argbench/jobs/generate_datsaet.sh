e410df654cba2a3bb9cc8abb81fed2095b3474a8
Yamen <y.ajjour@ai.uni-hannover.de>
2025-05-09 17:46:59 +0200

    -added seed

--- argbench/jobs/generate_datsaet.sh
+++ argbench/jobs/generate_datsaet.sh
@@ -1,5 +1,11 @@
 # Does not work for s
-BIGWORK=/bigwork/nhwpajjy/
+if [ -e "$file" ]; then
+  BIGWORK=/bigwork/nhwpajjy/
+  else
+  BIGWORK=/mnt/hme/yajjour
+fi
+
+
 export DATA_PATH="$BIGWORK/task-specific-argument-mining-and-generation-data"
 python "argbench/converter/convert_$1.py"
 python -m argbench.experiment.preprocess -t "$1" -o "${DATA_PATH}/tasks"
