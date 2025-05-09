daf0c1b0c2e0810514ec3c3b18cd1b4f1edab3e9
Yamen <y.ajjour@ai.uni-hannover.de>
2025-05-09 12:55:57 +0200

    checked if : in span

--- argbench/jobs/prompting_test.sh
+++ argbench/jobs/prompting_test.sh
@@ -6,7 +6,6 @@
 #SBATCH --time=24:00:00
 #SBATCH --output argbench/output/prmt-all-sample-%j.out
 #SBATCH --error argbench/output/prmt-all-sample-%j.err
-#SBATCH --partition ainlp
 #SBATCH --gpus=a100:1
 module load Miniforge3
 conda activate task-specific
