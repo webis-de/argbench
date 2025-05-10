eef2c2220f87b5523c7b1f46fabdcbe620ba2507
Yamen <y.ajjour@ai.uni-hannover.de>
2025-05-10 18:04:21 +0200

    catching decoding error

--- argbench/jobs/run_in_tasks_experiments.sh
+++ argbench/jobs/run_in_tasks_experiments.sh
@@ -11,6 +11,7 @@ module load Miniforge3
 conda activate task-specific
 
 model=$1
+dataset=$2
 echo $model
 
 
@@ -18,8 +19,11 @@ export CODE_PATH="$BIGWORK/task-specific-argument-mining-and-generation"
 export DATA_PATH="$BIGWORK/task-specific-argument-mining-and-generation-data"
 
 cd "$CODE_PATH"
-
-datasets=$(jq '.test[]'  "${CODE_PATH}/argbench/experiment/configs/experiment_splits.json")
+if [ -z "$dataset" ]; then
+  datasets=$(jq '.test[]'  "${CODE_PATH}/argbench/experiment/configs/experiment_splits.json")
+else
+  datasets=(dataset)
+fi
 for dataset in ${datasets[@]};
 do
 echo $dataset
@@ -29,5 +33,5 @@ python -m  argbench.experiment.run -c "${CODE_PATH}/argbench/experiment/configs/
 done
 
 export TIME="$(sacct --format=Elapsed -j $SLURM_JOB_ID | tail -n 1 | xargs 2>&1)"
-export JOB_ARGUMENTS="hpo;all-datasets-0.25;in-task;${model};\n"
+export JOB_ARGUMENTS="hpo;${datasets}.5;in-task;${model};\n"
 echo "$SLURM_JOB_ID,$SLURM_JOB_NAME,$TIME,$JOB_ARGUMENTS" >> "$CODE_PATH/argbench/jobs/job-accounting.csv"
