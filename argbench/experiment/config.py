1b32b18f8403c8280d7dff6d10390bb985424d27
Yamen <y.ajjour@ai.uni-hannover.de>
2025-05-12 17:49:08 +0200

    ## Dataset
    - switched to huggingface datasets to enable streaming
    - created functions to create and load the datasets for prompting and in-task experiments
    
    - distinguished between a model template and a prompt template
    
    - removed cot model template and few shot model template and replaced that with one template for each model
    ## Runner
    
    - adjusted the runner coder to do also tokenization and formatting in streaming
    
    - I dropped the automatic eos adding, since it should be added by default by the model ! Is this true?
    
    - I optimized the code by dropping two uneeded tokenizatiation before prompting
    
    - I switched to always use a train and val to optimize the model
    
    -

--- argbench/experiment/config.py
+++ argbench/experiment/config.py
@@ -312,9 +312,9 @@ class ModelConfig(CommonConfig):
     label: str
     path: str
     prompt_template: str
-    shot_prompt_template: str
+
     output_splitter: str
-    cot_prompt_template : str
+
     cutoff_len: int
 
 @dataclass
