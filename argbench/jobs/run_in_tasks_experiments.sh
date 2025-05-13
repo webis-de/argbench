9472e92c4eb4023dcaca70a010f2695afc74a3a7
Yamen <y.ajjour@ai.uni-hannover.de>
2025-05-13 07:22:36 +0200

    f - distinguished between a model template and a prompt template
    - removed cot model template and few shot model template and replaced that with one template for each model
    ## Runner
    
    - adjusted the runner coder to do also tokenization and formatting in streaming
    
    - I dropped the automatic eos adding, since it should be added by default by the model ! Is this true?
    
    - I optimized the code by dropping two uneeded tokenizatiation before prompting
    
    - I switched to always use a train and val to optimize the model
    
    -

--- argbench/jobs/run_in_tasks_experiments.sh
+++ argbench/jobs/run_in_tasks_experiments.sh
@@ -22,7 +22,7 @@ cd "$CODE_PATH"
 if [ -z "$dataset" ]; then
   datasets=$(jq '.test[]'  "${CODE_PATH}/argbench/experiment/configs/experiment_splits.json")
 else
-  datasets=(dataset)
+  datasets=("\"$dataset\"")
 fi
 for dataset in ${datasets[@]};
 do
