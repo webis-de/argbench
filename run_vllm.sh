76f3becdf2f9b9019d92e55d0e96b411cc8e5c4b
Yamen <y.ajjour@ai.uni-hannover.de>
2025-05-14 00:00:25 +0200

    switched to decoded

--- run_vllm.sh
+++ run_vllm.sh
@@ -2,11 +2,11 @@
 #SBATCH --job-name=prmt-all-data
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=12
-#SBATCH --mem=25G
+#SBATCH --mem=10G
 #SBATCH --time=12:00:00
 #SBATCH --output argbench/output/run_vllm.out
 #SBATCH --error argbench/output/run_vllm.err
-#SBATCH --gpus=a100:2
+#SBATCH --gpus=1
 module load Miniforge3
 conda activate few-shot-priming
 
