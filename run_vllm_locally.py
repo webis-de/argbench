dbf54f25506d64fd372e5bc7194c9ab3496a9995
Yamen <y.ajjour@ai.uni-hannover.de>
2025-05-14 00:10:15 +0200

    switched to decoded

--- run_vllm_locally.py
+++ run_vllm_locally.py
@@ -1,4 +1,4 @@
 from vllm import LLM, SamplingParams
-llm = LLM(model="/bigwork/nhwpajjy/pre-trained-models/DeepSeek-R1-Distill-Qwen1.5B")
+llm = LLM(model="/bigwork/nhwpajjy/pre-trained-models/DeepSeek-R1-Distill-Qwen-1.5B")
 sampling_params = SamplingParams(truncate_prompt_tokens=10)
 print(llm.generate("hello. i am not truncating you. you are truncating me. if we keep like this, we will truncate each other", sampling_params=sampling_params))
