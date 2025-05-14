4e641be3a62e2c59161256674422ce267494da7c
Yamen <y.ajjour@ai.uni-hannover.de>
2025-05-14 21:15:22 +0200

    switched to decoded

--- argbench/experiment/run.py
+++ argbench/experiment/run.py
@@ -595,7 +595,7 @@ class Runner:
         counter = 0
         for data in tqdm(loader):
             text = data["input"][0]
-            import pdb; pdb.set_trace()
+            #import pdb; pdb.set_trace()
             labels.extend(data["output"])
 
             if model:
