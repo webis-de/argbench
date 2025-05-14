5d59ecea2a4c3c67f1c4b53eb37fc3f8d298aae9
Yamen <y.ajjour@ai.uni-hannover.de>
2025-05-14 13:33:06 +0200

    switched to decoded

--- run_vllm_locally.py
+++ run_vllm_locally.py
@@ -1,4 +1,12 @@
 from vllm import LLM, SamplingParams
 llm = LLM(model="/bigwork/nhwpajjy/pre-trained-models/DeepSeek-R1-Distill-Qwen-1.5B")
-sampling_params = SamplingParams(truncate_prompt_tokens=2)
+sampling_params= {
+    "temperature": 0.8,
+    "top_p": 1,
+    "top_k": -1,
+    "max_tokens": 200,
+    "min_tokens": 50,
+    "truncate_prompt_tokens":2
+}
+sampling_params = SamplingParams(**sampling_params )
 print(llm.generate("what is the capital of france?", sampling_params=sampling_params))
