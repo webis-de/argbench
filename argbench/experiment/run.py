e1221b843bbfbb92841b16aa594c4ebd8879252a
Yamen <y.ajjour@ai.uni-hannover.de>
2025-05-15 11:05:57 +0200

    creating prompting jobs

--- argbench/experiment/run.py
+++ argbench/experiment/run.py
@@ -317,7 +317,7 @@ class Runner:
                 EarlyStoppingCallback(**self.config.early_stopping_config.to_conf(trial, early_stopping_hpo))
             )
         log_mem("preparing trainer")
-        trainer = Trainer(model=model, callbacks=callbacks, train_dataset=IterableWrapper(self.iterable_dataset["train"]), eval_dataset=IterableWrapper(self.iterable_dataset["val"]),
+        trainer = Trainer(model=model, callbacks=callbacks, train_dataset=self.iterable_dataset["train"].with_format("torch"), eval_dataset=self.iterable_dataset["val"].with_format("torch"),
         args=train_args, data_collator=data_collator,)
 
         return trainer
