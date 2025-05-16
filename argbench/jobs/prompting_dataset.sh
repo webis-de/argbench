282552c687d9e633d59aadf4a8d122445131744e
Yamen Ajjour <y.ajjour@ai.uni-hannover.de>
2025-05-16 11:54:43 +0200

    merging

--- argbench/jobs/prompting_dataset.sh
+++ argbench/jobs/prompting_dataset.sh
@@ -3,10 +3,10 @@
 #SBATCH --nodes=1 
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=10G
-#SBATCH --time=96:00:00
+#SBATCH --time=48:00:00
 #SBATCH --output argbench/output/prmt-dataset-%j.out
 #SBATCH --error argbench/output/prmt-dataset-%j.err
-#SBATCH --gpus=a100:1
+#SBATCH --gpus=1
 
 
 module load Miniforge3
