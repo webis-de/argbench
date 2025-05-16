3c10d8396b5f154fa5a9fa2a3c173629fd3c4800
Yamen <y.ajjour@ai.uni-hannover.de>
2025-05-16 15:19:12 +0200

    adjusting params

--- argbench/jobs/run_cross_tasks_experiments.sh
+++ argbench/jobs/run_cross_tasks_experiments.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 #SBATCH --job-name=cross-task-hpo
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=25G
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=32G
 #SBATCH --time=48:00:00
 #SBATCH --output argbench/output/cross-task-hpo-%j.out
 #SBATCH --error argbench/output/cross-task-hpo-%j.err
