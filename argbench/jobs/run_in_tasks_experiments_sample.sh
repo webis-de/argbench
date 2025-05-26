d52946efa70dfb05feb5bf6ea2eb7f276c93e9d4
Yamen <y.ajjour@ai.uni-hannover.de>
2025-05-26 11:55:16 +0200

    - implemented cross task final

--- argbench/jobs/run_in_tasks_experiments_sample.sh
+++ argbench/jobs/run_in_tasks_experiments_sample.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=10G
-#SBATCH --time=48:00:00
+#SBATCH --time=72:00:00
 #SBATCH --output argbench/output/in-task-hpo-%j.out
 #SBATCH --error argbench/output/in-task-hpo-%j.err
 #SBATCH --gpus=1
