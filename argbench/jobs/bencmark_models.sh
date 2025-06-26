a2215b799accb40ba37bbf00cabfc4d97691f5f4
Yamen <y.ajjour@ai.uni-hannover.de>
2025-06-26 17:23:02 +0200

    - implemented cross task final

--- argbench/jobs/bencmark_models.sh
+++ argbench/jobs/bencmark_models.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
-#SBATCH --job-name=open_debate_evidence
+#SBATCH --job-name=benchmarking_models
 #SBATCH --nodes=1 
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=12G
 #SBATCH --time=24:00:00
-#SBATCH --output argbench/output/open_debate_evidence_output.out
-#SBATCH --error argbench/output/open_debate_evidence_error.err
+#SBATCH --output argbench/output/benchmarking_models.out
+#SBATCH --error argbench/output/benchmarking_models.err
 #SBATCH --gpus=1
 
 export CODE_PATH="$BIGWORK/task-specific-argument-mining-and-generation"
