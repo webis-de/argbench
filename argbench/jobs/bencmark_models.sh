51f1987a3cf5e729a7fed80a625ea4908b0e02b0
Yamen <y.ajjour@ai.uni-hannover.de>
2025-06-26 17:12:26 +0200

    - implemented cross task final

---
+++ argbench/jobs/bencmark_models.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+#SBATCH --job-name=open_debate_evidence
+#SBATCH --nodes=1 
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=18G
+#SBATCH --time=24:00:00
+#SBATCH --output argbench/output/open_debate_evidence_output.out
+#SBATCH --error argbench/output/open_debate_evidence_error.err
+#SBATCH --gpus=1
+
+export CODE_PATH="$BIGWORK/task-specific-argument-mining-and-generation"
+
+module load Miniforge3
+conda activate task-specific
+python "$CODE_PATH/benchmark_performance.py"
