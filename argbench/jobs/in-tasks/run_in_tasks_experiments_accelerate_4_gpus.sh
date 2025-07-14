512fff2337983f1975b1fd42489cb81f24396958
Yamen <y.ajjour@ai.uni-hannover.de>
2025-07-14 14:37:51 +0200

    - adding adamw_torch

--- argbench/jobs/in-tasks/run_in_tasks_experiments_accelerate_4_gpus.sh
+++ argbench/jobs/in-tasks/run_in_tasks_experiments_accelerate_4_gpus.sh
@@ -28,7 +28,7 @@ for dataset in ${datasets[@]};
 do
 echo $dataset
 
-accelerate launch --config_file "${CODE_PATH}/argbench/experiment/configs/accelerate/config_2_gpus_4_stage.yaml" \
+accelerate launch --config_file "${CODE_PATH}/argbench/experiment/configs/accelerate/config_4_gpus_2_stage.yaml" \
  -m  argbench.experiment.run -c "${CODE_PATH}/argbench/experiment/configs/hpo/in_task_hpo_${dataset:1:-1}.json" \
 --base_model "$model"  --debug --sample --train_optim "adamw_torch"
 
