610805c96590af39aeb90caa2148112f5e5dc2ac
Yamen <y.ajjour@ai.uni-hannover.de>
2025-07-14 13:54:23 +0200

    - parameterized quantization

--- argbench/jobs/in-tasks/run_in_tasks_experiments_accelerate.sh
+++ argbench/jobs/in-tasks/run_in_tasks_experiments_accelerate.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=72:00:00
 #SBATCH --output argbench/output/in-task-hpo-%j.out
 #SBATCH --error argbench/output/in-task-hpo-%j.err
-#SBATCH --gpus=h100:4
+#SBATCH --gpus=h100:2
 module load Miniforge3
 conda activate task-specific-new
 
