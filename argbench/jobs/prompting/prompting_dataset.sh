61580d27004baaa4b5c9e3f6b8d20dc3c24a8d2b
Yamen <y.ajjour@ai.uni-hannover.de>
2025-07-16 14:28:41 +0200

    - cleaning predictions

--- argbench/jobs/prompting/prompting_dataset.sh
+++ argbench/jobs/prompting/prompting_dataset.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-echo "$1 $2 $3"
+echo "$1 $2 $3 $4"
 export CODE_PATH="$BIGWORK/task-specific-argument-mining-and-generation"
 export DATA_PATH="$BIGWORK/task-specific-argument-mining-and-generation-data"
-export model=$2
-export dataset=$1
 
+
+export dataset=$1
+export model=$2
+export gpu_count=$3
+export prompting_strategy=$4
 sbatch <<EOT
 #!/bin/bash -l
 #SBATCH --job-name=prmt-dataset
@@ -14,7 +17,7 @@ sbatch <<EOT
 #SBATCH --time=72:00:00
 #SBATCH --output argbench/output/prmt-dataset-%j.out
 #SBATCH --error argbench/output/prmt-dataset-%j.err
-#SBATCH --gpus="$3"
+#SBATCH --gpus="$gpu_count"
 
 
 module load Miniforge3
@@ -22,6 +25,6 @@ conda activate task-specific
 
 
 python -m  argbench.experiment.run -c "${CODE_PATH}/argbench/experiment/configs/prompting/prompting.json" \
---leaderboard-path "${DATA_PATH}/runs/prompting-$model-complete-results.csv" --base_model "$model" --test_dataset_name "$dataset" --debug
+--leaderboard-path "${DATA_PATH}/runs/prompting-$model-complete-results.csv" --base_model "$model" --test_dataset_name "$dataset" --debug "prompting_strategy"
 
 EOT
