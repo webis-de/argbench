f20ced939ba19509a55d279e730f82d46ced01fa
Yamen Ajjour <y.ajjour@ai.uni-hannover.de>
2025-07-16 13:12:26 +0200

    merging files

--- argbench/jobs/prompting/prompting_dataset.sh
+++ argbench/jobs/prompting/prompting_dataset.sh
@@ -52,9 +52,6 @@ done
 export TIME="$(sacct --format=Elapsed -j $SLURM_JOB_ID | tail -n 1 | xargs 2>&1)"
 export JOB_ARGUMENTS="prompting;${dataset};${model};\n"
 echo "$SLURM_JOB_ID,$SLURM_JOB_NAME,$TIME,$JOB_ARGUMENTS" >> "$CODE_PATH/argbench/jobs/job-accounting.csv"
-<<<<<<< HEAD
-=======
 
 exit 0
 EOT
->>>>>>> 3da6bf0c05e12b68ac25d2dfb2d59d66f3434e32
