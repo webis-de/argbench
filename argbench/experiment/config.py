30cf3215d18e8fe1bee79b8aeddb007e3ba22b20
Yamen <y.ajjour@ai.uni-hannover.de>
2025-07-16 14:51:52 +0200

    - cleaning predictions

--- argbench/experiment/config.py
+++ argbench/experiment/config.py
@@ -434,6 +434,7 @@ class RunConfig:
         """
         arg_parser.add_argument("--quantization", action="store_true")
         arg_parser.add_argument("--sample", action="store_true")
+
         arg_parser.add_argument("-icot", "--chain_of_thoughts", action="store_true")
         arg_parser.add_argument("-int", "--in_task", type=bool, help="whether to conduct a cross task or in task experiment")
         arg_parser.add_argument("-sf", "--skill_filter", type=str, help="filter the tasks based on skill")
