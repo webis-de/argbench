482d68db8fe8ffdd4607de612a146cd951b21fe2
Yamen <y.ajjour@ai.uni-hannover.de>
2025-07-16 13:12:13 +0200

    - cleaning predictions

--- argbench/jobs/prompting/prompting_dataset.sh
+++ argbench/jobs/prompting/prompting_dataset.sh
@@ -52,6 +52,4 @@ done
 export TIME="$(sacct --format=Elapsed -j $SLURM_JOB_ID | tail -n 1 | xargs 2>&1)"
 export JOB_ARGUMENTS="prompting;${dataset};${model};\n"
 echo "$SLURM_JOB_ID,$SLURM_JOB_NAME,$TIME,$JOB_ARGUMENTS" >> "$CODE_PATH/argbench/jobs/job-accounting.csv"
-
-exit 0
 EOT
