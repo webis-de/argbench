d25fded64bfea7c6c3260310ad78e0013b0983ae
Yamen <y.ajjour@ai.uni-hannover.de>
2025-07-18 17:44:57 +0200

    - in task experiments

--- argbench/jobs/in-tasks/run_in_tasks_experiments.sh
+++ argbench/jobs/in-tasks/run_in_tasks_experiments.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 echo "${@:4}"
 export CODE_PATH="$BIGWORK/task-specific-argument-mining-and-generation"
-export CONFIG_PATH="$BIGWORK/task-specific-argument-mining-and-generation/argbench/experiment/configs/in_task/"
+export CONFIG_PATH="$BIGWORK/task-specific-argument-mining-and-generation/argbench/experiment/configs/"
 export DATA_PATH="$BIGWORK/task-specific-argument-mining-and-generation-data"
 export gpu_count=$1
 export experiment=$2
 export jobname=$3
 
 if [ "$gpu_count" == 1 ] ; then
-
 sbatch <<EOT
 #!/bin/bash -l
 #SBATCH --job-name="$jobname"
@@ -23,9 +22,10 @@ sbatch <<EOT
 module load Miniforge3
 conda activate task-specific-new
 
-python -m  argbench.experiment.run -c "${CONFIG_PATH}/${experiment}.json" ${@:4}
+python -m  argbench.experiment.run -c "${CONFIG_PATH}/in_task/${experiment}.json" ${@:4}
 EOT
 else
+  echo "on accelerate"
 sbatch <<EOT
 #!/bin/bash -l
 #SBATCH --job-name="$jobname"
@@ -41,6 +41,6 @@ module load Miniforge3
 conda activate task-specific
 
 accelerate launch --config_file "${CONFIG_PATH}/accelerate/config_${gpu_count}_gpus_2_stage.yaml" \\
--m  argbench.experiment.run -c "${CONFIG_PATH}/${experiment}.json" ${@:4}
+-m  argbench.experiment.run -c "${CONFIG_PATH}/in_task/${experiment}.json" ${@:4}
 EOT
 fi;
