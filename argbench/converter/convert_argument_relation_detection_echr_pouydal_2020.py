22bfa5b57f27d29f15cb7ca5d9453bb0658b6751
Yamen <y.ajjour@ai.uni-hannover.de>
2025-08-06 17:15:53 +0200

    - preprocessed echr

--- argbench/converter/convert_argument_relation_detection_echr_pouydal_2020.py
+++ argbench/converter/convert_argument_relation_detection_echr_pouydal_2020.py
@@ -7,7 +7,24 @@ from nltk.tokenize.punkt import PunktTrainer, PunktSentenceTokenizer
 
 from argbench.converter.archive.convert_argument_unit_segmentation_echr import get_stanza_sentence_segmenter
 from argbench.converter.common import Genres, Output, Skills, datasets_path, Metadata, split_test_val_train
-from argbench.converter.convert_argument_unit_segmentation_echr import *
+import stanza
+
+
+def get_stanza_sentence_segmenter():
+    nlp = stanza.Pipeline(lang='en', processors='tokenize')
+
+    def segment_sentences_stanza(case_text: str):
+        doc = nlp(case_text)
+        sentence_indices = []
+
+        for sent in doc.sentences:
+
+            sentence_indices.append((sent.tokens[0].start_char, sent.tokens[-1].end_char))
+
+        return sentence_indices
+
+    return segment_sentences_stanza
+
 
 DATASET_NAME = "argument_relation_detection_echr_pouydal_2020"
 DATASET_FILE_TEMPLATE = "argument_relation_detection_echr_{split}_pouydal_2020.json"
