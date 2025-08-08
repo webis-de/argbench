2501136021b7737c1f8f4a79f969cee0355faddb
Yamen <y.ajjour@ai.uni-hannover.de>
2025-08-08 16:38:27 +0200

    factoring out echr functions

--- argbench/converter/convert_argument_rating_dagstuhl_15512_wachsmuth17.py
+++ argbench/converter/convert_argument_rating_dagstuhl_15512_wachsmuth17.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from common import Genres, Output, Skills, datasets_path, read_tabular, tasks_path, Metadata, add_seed_arg, set_seed, find_topic_size_to_split
 from argparse import ArgumentParser
 import random
@@ -6,28 +8,45 @@ import uuid
 
 p = re.compile(r"\(([a-zA-Z]+)\)")
 QUALITY_SCORES = ["Low", "Average", "High"]
-DATASET_NAME = "argument_rating_dagstuhl_15512_{dimension}_wachsmuth17"
+DATASET_NAME = "argument_rating_dagstuhl_15512_wachsmuth17"
 
-def  aggregate_labels_and_split(dataset, column):
-    arguments = dataset.groupby(["argument", "issue"])[["argument", "issue", column]].apply(lambda row: row[column].value_counts().index[0]).reset_index()
+def aggregate_labels_and_split(dataset, tasks):
+    all_arguments_dfs_aggregated = []
+    for task_name, description in tasks:
 
-    df_test, df_train = find_topic_size_to_split(arguments, "issue", 0.2)
+        arguments = dataset.groupby(["argument", "issue", "#id"])[["argument", "issue", "#id", task_name]].apply(lambda row: row[task_name].value_counts().index[0]).reset_index()
+        arguments["quality-aspect"] = task_name
+        arguments["quality-aspect-description"] = description
+        all_arguments_dfs_aggregated.append(arguments)
+    df_arguments_aggregated = pd.concat(all_arguments_dfs_aggregated)
+    df_argument_ids = dataset[["#id", "issue"]].drop_duplicates()
+    df_test, df_train = find_topic_size_to_split(df_argument_ids, "issue", 0.2)
     df_val, df_train = find_topic_size_to_split(df_train, "issue", 0.25)
+    all_dis = len(dataset["argument"].drop_duplicates())
+    print(f"all ids {all_dis}")
+    print(f"all {len(df_argument_ids)}")
+    print(f" test {len(df_test)}")
+    print(f" val {len(df_val)}")
+    print(f"train {len(df_train)}")
+    df_test = df_arguments_aggregated[df_arguments_aggregated["#id"].isin(df_test["#id"])]
+    df_train = df_arguments_aggregated[df_arguments_aggregated["#id"].isin(df_train["#id"])]
+    df_val = df_arguments_aggregated[df_arguments_aggregated["#id"].isin(df_val["#id"])]
 
     return df_test, df_val, df_train
 
-def make_output(dataset, metadata, column, aspect_description, dataset_file, split):
-    dimension = column.replace(' ', '_')
-    dataset_name = DATASET_NAME.format(dimension= dimension)
-    output = Output(dataset_name)
-    output.append_definition(f"""Judge the quality of argument according to quality aspect: {column}. 
-                            Quality aspect description: {aspect_description} Possible outputs:
+def make_output(dataset, metadata, dataset_file, split):
+
+    output = Output(DATASET_NAME)
+    output.append_definition(f"""Judge the quality of the argument according to quality aspect. Possible outputs:
                             Low if arguments aspect quality is low, Average if argument's aspect quality is average,
                             High if arguments aspect quality is high.""")
 
     for row in dataset.iterrows():
         row = row[1]
-        prompt = f"Argument: {row['argument']}"
+        quality_aspect = row["quality-aspect"]
+        quality_aspect_description = row["quality-aspect-description"]
+        prompt = f"Argument: {row['argument']}\nQuality Aspect: {quality_aspect}\nQuality Aspect Definition: {quality_aspect_description}"
+
         response = p.findall(row[0])[0]
         id = str(uuid.uuid4())
 
@@ -42,8 +61,6 @@ if __name__ == "__main__":
     arg_parser = ArgumentParser()
     add_seed_arg(arg_parser)
     args = arg_parser.parse_known_args()[0]
-    set_seed(args)
-
 
     dataset_path = str(datasets_path()
                        / "dagstuhl-15512-argquality-corpus-v2"
@@ -85,22 +102,26 @@ if __name__ == "__main__":
         ("emotional appeal", emotional_appeal_description),
         ("sufficiency", local_sufficiency_description)
     ]
-    for task_name, description in tasks:
+
+    metadata = Metadata(DATASET_NAME)
+    test_filename = f"argument_rating_dagstuhl_15512_test_wachsmuth17.json"
+    train_filename = f"argument_rating_dagstuhl_15512_train_wachsmuth17.json"
+    val_filename = f"argument_rating_dagstuhl_15512_val_wachsmuth17.json"
+
+    df_test, df_val, df_train = aggregate_labels_and_split(dataset, tasks)
+    print(f"length of test is {len(df_test)}")
+    print(f"length of train is {len(df_train)}")
+    print(f"length of val is {len(df_val)}")
+    make_output(df_test, metadata,  test_filename, "test")
+    make_output(df_val, metadata,  val_filename, "val")
+    make_output(df_train, metadata, train_filename, "train")
 
 
-        dataset_name = DATASET_NAME.format(dimension= task_name.replace(' ', '_'))
-        metadata = Metadata(dataset_name)
-        df_test, df_val, df_train = aggregate_labels_and_split(dataset, task_name)
 
-        test_filename = f"argument_rating_dagstuhl_15512_{task_name.replace(' ', '_')}_test_wachsmuth17.json"
-        train_filename = f"argument_rating_dagstuhl_15512_{task_name.replace(' ', '_')}_train_wachsmuth17.json"
-        val_filename = f"argument_rating_dagstuhl_15512_{task_name.replace(' ', '_')}_val_wachsmuth17.json"
 
-        make_output(df_test, metadata, task_name, description, test_filename, "test")
-        make_output(df_val, metadata, task_name, description, val_filename, "val")
-        make_output(df_train, metadata, task_name, description, train_filename, "train")
 
-        metadata.add_evaluation_metric("fscore")
-        metadata.add_genre(Genres.DEBATE_PORTALS)
-        metadata.add_skill(Skills.QUALITY_ASSESSMENT)
-        metadata.write_metadata()
+
+    metadata.add_evaluation_metric("fscore")
+    metadata.add_genre(Genres.DEBATE_PORTALS)
+    metadata.add_skill(Skills.QUALITY_ASSESSMENT)
+    metadata.write_metadata()
