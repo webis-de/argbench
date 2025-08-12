ec3505319143c9eb41a7d40a28e9790396819345
Yamen <y.ajjour@ai.uni-hannover.de>
2025-08-12 17:45:51 +0200

    fixed metadata problem for argument relation detection

---
+++ argbench/jobs/test_job_eof.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+export CODE_PATH="$BIGWORK/task-specific-argument-mining-and-generation"
+sbatch << EOF
+#!/bin/bash -l
+#SBATCH --job-name=test-job
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=30G
+#SBATCH --time=72:00:00
+#SBATCH --output argbench/output/test-job-%j.out
+#SBATCH --error argbench/output/test-job-%j.err
+#SBATCH --partition tnt
+
+module load Miniforge3
+conda activate task-specific
+echo "working"
+start=\$(date +%s)
+echo "start"
+echo "\$start"
+python -c "[print(i) for i in range(10000000)]"
+end=\$(date +%s)
+echo "end"
+echo "\$end"
+export TIME=\$((end-start))
+echo "\$Time"
+echo "\$TIME,jobname" >> "$CODE_PATH/argbench/jobs/job-accounting.csv"
+EOF
--- argbench/jobs/test_job_eof_double_quoted.sh
+++ argbench/jobs/test_job_eof_double_quoted.sh
@@ -23,6 +23,6 @@ end=$(date +%s)
 echo "end"
 echo "$end"
 export TIME=$((end-start))
-echo $TIME
+echo $Time
 echo "$TIME,jobname" >> "$CODE_PATH/argbench/jobs/job-accounting.csv"
 EOF
