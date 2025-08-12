e077ba3652b0bd54a2a4b5eb7960b6f992f57e6d
Yamen <y.ajjour@ai.uni-hannover.de>
2025-08-12 16:58:13 +0200

    fixed metadata problem for argument relation detection

---
+++ argbench/jobs/test_job.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+export CODE_PATH="$BIGWORK/task-specific-argument-mining-and-generation"
+
+sbatch <<EOT
+#!/bin/bash -l
+#SBATCH --job-name=test-job
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=30G
+#SBATCH --time=72:00:00
+#SBATCH --output argbench/output/test-job-%j.out
+#SBATCH --error argbench/output/test-job-%j.err
+#SBATCH --gpus=1
+
+
+module load Miniforge3
+conda activate new-env
+
+start=\$(date +%s)
+python -c "[_ for i in range(10000000)]"
+python -c
+end=\$(date +%s)
+export TIME=\$((end-start))
+echo "$TIME,"jobname" >> "$CODE_PATH/argbench/jobs/job-accounting.csv"
+EOT
