d998ef138e9c046343f3d1af975cd66ec6d19a94
Yamen <y.ajjour@ai.uni-hannover.de>
2025-08-25 18:14:42 +0200

    - finishing input conslidation

--- argbench/jobs/prompting/prompting.sh
+++ argbench/jobs/prompting/prompting.sh
@@ -15,7 +15,7 @@ sbatch <<EOF
 #SBATCH --time=72:00:00
 #SBATCH --output argbench/output/"$jobname"-%j.out
 #SBATCH --error argbench/output/"$jobname"-%j.err
-#SBATCH --gres=gpu:a100:"$gpu_count"
+#SBATCH --gres=gpu:h100:"$gpu_count"
 
 module load Miniforge3
 conda activate new-env
