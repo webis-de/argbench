e4388bb73151c4dc4e088b945369fe4469f14653
Yamen <y.ajjour@ai.uni-hannover.de>
2025-08-26 16:37:46 +0200

    - finishing input conslidation

---
+++ argbench/experiment/tests/test_cleaning_predictions.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+from argbench.experiment.run import *
+
+class test_response_cleaning(TestCase):
+    def test_response_cleaning(self):
+        output = "Different Topics</think> </think> Different Topics</think> </think> Different Topics."
+        response = clean_prediction(output, False)
+        print(f"response response")
--- argbench/jobs/prompting/prompting_all_tasks.sh
+++ argbench/jobs/prompting/prompting_all_tasks.sh
@@ -27,12 +27,12 @@ else
 fi
 
 for model in "${models[@]}"; do
-    bash argbench/jobs/prompting/prompting.sh 1 prompting "${model:0:3}-0-shot" --max-length 128000 --model "$model" --debug --sample
+    bash argbench/jobs/prompting/prompting.sh 1 prompting "${model:0:3}-0-shot" --max_length 128000 --model "$model" --debug --sample
     sleep 5
-    bash argbench/jobs/prompting/prompting.sh 1 prompting "${model:0:3}-1-shot" --max-length 128000  --model "$model" -k 1 --debug --sample
+    bash argbench/jobs/prompting/prompting.sh 1 prompting "${model:0:3}-1-shot" --max_length 128000  --model "$model" -k 1 --debug --sample
     sleep 5
-    bash argbench/jobs/prompting/prompting.sh 1 prompting "${model:0:3}-4-shot" --max-length 128000  --model "$model" -k 4 --debug --sample
+    bash argbench/jobs/prompting/prompting.sh 1 prompting "${model:0:3}-4-shot" --max_length 128000  --model "$model" -k 4 --debug --sample
     sleep 5
-    bash argbench/jobs/prompting/prompting.sh 1 prompting "${model:0:3}-cot" --max-length 128000 --model "$model" --cot --debug --sample
+    bash argbench/jobs/prompting/prompting.sh 1 prompting "${model:0:3}-cot" --max_length 128000 --model "$model" --cot --debug --sample
     sleep 5
 done
