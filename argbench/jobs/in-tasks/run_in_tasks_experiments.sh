2af49fdf3f80aa4f0f437bbe3dc7c8d20f96d9d6
Yamen <y.ajjour@ai.uni-hannover.de>
2025-08-27 17:32:26 +0200

    - finishing input conslidation

--- argbench/jobs/in-tasks/run_in_tasks_experiments.sh
+++ argbench/jobs/in-tasks/run_in_tasks_experiments.sh
@@ -22,7 +22,7 @@ sbatch <<EOF
 #SBATCH --gres=gpu:"$gpu_type:$gpu_count"
 
 module load Miniforge3
-conda activate task-specific-new
+conda activate argbench
 
 export HF_HUB_OFFLINE=1
 start=\$(date +%s)
@@ -51,7 +51,7 @@ sbatch <<EOF
 #SBATCH --gpus="$gpu_type:$gpu_count"
 
 module load Miniforge3
-conda activate task-specific-new
+conda activate argbench
 start=\$(date +%s)
 Start_Date=\$(date +%Y-%m-%d:%H:%m)
 echo "parallel"
