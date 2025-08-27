47a53e2ccda14300810a6ecf1af0bad000196042
Yamen <y.ajjour@ai.uni-hannover.de>
2025-08-27 14:49:58 +0200

    - finishing input conslidation

--- argbench/jobs/test_bert_score.sh
+++ argbench/jobs/test_bert_score.sh
@@ -12,4 +12,5 @@ export CODE_PATH="$BIGWORK/task-specific-argument-mining-and-generation"
 
 module load Miniforge3
 conda activate new-env
+python -c "from transformers import AutoTokenizer; AutoTokenizer.from_pretrained('roberta-large')"
 python -m unittest argbench.experiment.tests.test_bert_score
