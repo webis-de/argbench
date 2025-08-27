028c31e134a3ac0e64890c3b1868801387d8f00f
Yamen <y.ajjour@ai.uni-hannover.de>
2025-08-27 13:57:42 +0200

    - finishing input conslidation

--- argbench/jobs/prompting/prompting_all_tasks.sh
+++ argbench/jobs/prompting/prompting_all_tasks.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 
-while getopts "m:" opt; do
+while getopts "ahm:" opt; do
   case $opt in
+    a)
+      gpu_type="a100"
+    ;;
+    h)
+      gpu_type="h100"
+    ;;
+
     m)
       model="$OPTARG"
       ;;
@@ -27,12 +34,12 @@ else
 fi
 
 for model in "${models[@]}"; do
-    bash argbench/jobs/prompting/prompting.sh 1 "a100" prompting "${model:0:3}-0-shot" --max_length 128000 --model "$model" --debug --sample
+    bash argbench/jobs/prompting/prompting.sh 1 "$gpu_type" prompting "${model:0:3}-0-shot" --max_length 128000 --model "$model" --debug --sample
     sleep 5
-    bash argbench/jobs/prompting/prompting.sh 1 "a100" prompting "${model:0:3}-1-shot" --max_length 128000  --model "$model" -k 1 --debug --sample
+    bash argbench/jobs/prompting/prompting.sh 1 "$gpu_type" prompting "${model:0:3}-1-shot" --max_length 128000  --model "$model" -k 1 --debug --sample
     sleep 5
-    bash argbench/jobs/prompting/prompting.sh 1 "a100" prompting "${model:0:3}-4-shot" --max_length 128000  --model "$model" -k 4 --debug --sample
+    bash argbench/jobs/prompting/prompting.sh 1 "$gpu_type" prompting "${model:0:3}-4-shot" --max_length 128000  --model "$model" -k 4 --debug --sample
     sleep 5
-    bash argbench/jobs/prompting/prompting.sh 1 "a100" prompting "${model:0:3}-cot" --max_length 128000 --model "$model" --cot --debug --sample
+    bash argbench/jobs/prompting/prompting.sh 1 "$gpu_type" prompting "${model:0:3}-cot" --max_length 128000 --model "$model" --cot --debug --sample
     sleep 5
 done
