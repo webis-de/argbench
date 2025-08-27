b28f0dbf3a73da22dd722e075e6b980a64af0e82
Yamen <y.ajjour@ai.uni-hannover.de>
2025-08-27 14:18:03 +0200

    - finishing input conslidation

--- argbench/experiment/run.py
+++ argbench/experiment/run.py
@@ -93,9 +93,13 @@ def tokenize(prompt, tokenizer, cutoff_len, train):
     if train:
         #prompt = tokenizer(prompt, padding="max_length", max_length=cutoff_len, truncation=True)
         prompt = tokenizer(prompt, padding=True, max_length=cutoff_len, truncation=True)
+        logger.debug( f"count of tokens in prompt {len(prompt['input_ids'])}")
     else:
+
         #prompt =  tokenizer(prompt, padding="max_length", max_length=cutoff_len, truncation=True, return_tensors="pt")
         prompt =  tokenizer(prompt, padding=True, max_length=cutoff_len, truncation=True, return_tensors="pt")
+
+        logger.debug( f"count of tokens in prompt {len(prompt['input_ids'])}")
         return prompt
     if prompt["input_ids"][-1] != tokenizer.eos_token_id and len(prompt["input_ids"]) < cutoff_len:
         prompt["input_ids"].append(tokenizer.eos_token_id)
