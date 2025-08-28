749711442abf9923a1b9d27cca51628c1c706e7f
Yamen <y.ajjour@ai.uni-hannover.de>
2025-08-28 17:37:34 +0200

    - switched sampling to 1000 maximum per dataset

--- argbench/jobs/in-tasks/run_in_tasks_experiments.sh
+++ argbench/jobs/in-tasks/run_in_tasks_experiments.sh
@@ -22,7 +22,7 @@ sbatch <<EOF
 #SBATCH --gres=gpu:"$gpu_type:$gpu_count"
 
 module load Miniforge3
-conda activate new-env
+conda activate argbench
 export CUDA_VISIBLE_DEVICES=0
 export HF_HUB_OFFLINE=1
 start=\$(date +%s)
