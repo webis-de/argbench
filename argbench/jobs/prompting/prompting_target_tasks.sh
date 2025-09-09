af88d641585b37c743cc745ca82c044aa684e671
Yamen <y.ajjour@ai.uni-hannover.de>
2025-09-09 16:48:43 +0200

    - fixed cleaning prediction to use a regular expression

--- argbench/jobs/prompting/prompting_target_tasks.sh
+++ argbench/jobs/prompting/prompting_target_tasks.sh
@@ -50,8 +50,8 @@ for model in "${models[@]}"; do
 #    sleep 5
 #    bash argbench/jobs/prompting/prompting.sh 1 "$gpu_type" prompting "${task:0:3}-${model:0:3}-4-shot" --dataset "$task" --model "$model" -k 4 --debug --sample
 #    sleep 5
-#    bash argbench/jobs/prompting/prompting.sh 1 "$gpu_type" prompting "${task:0:3}-${model:0:3}-cot" --dataset "$task" --model "$model" --cot --debug --sample
-#    sleep 5
+    bash argbench/jobs/prompting/prompting.sh 1 "$gpu_type" prompting "${task:0:3}-${model:0:3}-cot" --dataset "$task" --model "$model" --cot --debug --sample
+    sleep 5
   done
 
 done
