c7bb98ad7615e0173b543052a18e5e57ce525759
Yamen <y.ajjour@ai.uni-hannover.de>
2025-09-10 10:12:37 +0200

    - fixed cleaning prediction to use a regular expression

--- argbench/experiment/run.py
+++ argbench/experiment/run.py
@@ -75,24 +75,20 @@ def clean_prediction_general(prediction):
 
 def clean_for_generation_and_segmentation(prediction, chain_of_thoughts):
     prediction = clean_prediction_general(prediction)
-    if chain_of_thoughts and "Output:" in prediction:
+    ## Output is the main indicator of output
+    if "Output:" in prediction:
         index = prediction.rindex("Output:")
         prediction = prediction[index+7:]
+    elif "output:" in prediction:
+        index = prediction.rindex("output:")
+        prediction = prediction[index+7:]
+    ## Dorp thinking processes
     prediction = re.sub("<think>.*</think>", "", prediction)
-    if prediction.count("</think>") > 1:
-        index = prediction.rindex("</think>")
-        prediction = prediction[index+8:]
-        if len(prediction.strip()) >=3:
-            return prediction
-        elif prediction.index("</think>") >= 3:
-            ind = prediction.index("</think>")
-            return prediction[:ind]
-    if "</think>" in prediction:
-        cleaned_prediction = prediction.split("</think>")[1]
-        if len(cleaned_prediction.strip()) >=3:
-            return cleaned_prediction
-        elif len(prediction.split("</think>")[0]) >= 3:
-            return prediction.split("</think>")[0]
+
+    ## drop unjustified thinking tokens
+    if "</think>" in prediction :
+        prediction = prediction.replace("</think>", "")
+
     return prediction
 
 def clean_for_classification(prediction, labels):
