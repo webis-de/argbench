93acb435e7071f016ce166b6fadeaf2eecf22fb6
Yamen <y.ajjour@ai.uni-hannover.de>
2025-09-18 06:35:00 +0200

    updating README.md

--- argbench/jobs/prompting/prompting_all_tasks.sh
+++ argbench/jobs/prompting/prompting_all_tasks.sh
@@ -28,16 +28,16 @@ done
 
 
 if [ -z "$model" ]; then
-  models=("phi-3.5-moe-7.5b" "qwen3-4b" "deepseek-r1-distill-7b" "llama-3.1-8b-instruct" "gemma3-4b")
+  models=("phi-3.5-moe-7.6b" "qwen3-4b" "deepseek-r1-distill-7b" "llama-3.1-8b-instruct" "gemma3-4b")
 else
   models=("$model")
 fi
 
 for model in "${models[@]}"; do
-    bash argbench/jobs/prompting/prompting.sh 1 "$gpu_type" prompting "${model:0:3}-0-shot"  --model "$model" --debug --sample --seed 1517
-    sleep 5
-    bash argbench/jobs/prompting/prompting.sh 1 "$gpu_type" prompting "${model:0:3}-4-shot"  --model "$model" -k 4 --debug --sample --seed 1517
-    sleep 5
+#    bash argbench/jobs/prompting/prompting.sh 1 "$gpu_type" prompting "${model:0:3}-0-shot"  --model "$model" --debug --sample --seed 1517
+#    sleep 5
+#    bash argbench/jobs/prompting/prompting.sh 1 "$gpu_type" prompting "${model:0:3}-4-shot"  --model "$model" -k 4 --debug --sample --seed 1517
+#    sleep 5
     bash argbench/jobs/prompting/prompting.sh 1 "$gpu_type" prompting "${model:0:3}-cot" --model "$model" --cot --debug --sample --seed 1517
     sleep 5
 done
