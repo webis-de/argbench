dd237593db47e4efdffd49736331b3b8db9bba1b
Yamen <y.ajjour@ai.uni-hannover.de>
2025-09-19 14:53:51 +0200

    updating README.md

--- argbench/jobs/in-tasks/run_in_task_all.sh
+++ argbench/jobs/in-tasks/run_in_task_all.sh
@@ -66,6 +66,8 @@ for model in "${models[@]}"; do
       if [ "$gpu_count" == 1 ] ; then
           bash argbench/jobs/in-tasks/run_in_tasks_experiments.sh "$gpu_count" "$gpu_type" "in_task_${task}_hpo" "in-tsk-$model-$task-hpo" --model "$model" --debug  --quantization --sample
         else
+          echo "$gpu_count"
+          echo "$gpu_type"
           bash argbench/jobs/in-tasks/run_in_tasks_experiments.sh "$gpu_count" "$gpu_type" "in_task_${task}_hpo" "in-tsk-$model-$task-hpo" --model "$model" --debug  --sample --train_optim "adamw_torch"
         fi
 
