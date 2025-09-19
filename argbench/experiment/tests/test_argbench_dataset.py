1b311cea32c7f6c4345841495105fb6392bf3714
Yamen <y.ajjour@ai.uni-hannover.de>
2025-09-19 14:41:11 +0200

    updating README.md

--- argbench/experiment/tests/test_argbench_dataset.py
+++ argbench/experiment/tests/test_argbench_dataset.py
@@ -8,10 +8,16 @@ from transformers import AutoTokenizer
 
 class testArgBenchDataset(TestCase):
     def test_dataset_size(self):
-        main_path = "/bigwork/nhwpajjy/task-specific-argument-mining-and-generation-data/argbench-dataset"
+        main_path = "/mnt/home/yajjour/task-specific-argument-mining-and-generation-data/argbench-dataset"
         argbench_dataset = f"{main_path}/argbench-prompting-zero-shot-small"
         dataset = load_from_disk(argbench_dataset)
-        self.assertEquals(dataset["test_counter_argument_generation_candela_hua19"].shape[0], 1000)
+        self.assertEquals(dataset["test_counter_argument_generation_candela_hua19"].shape[0], 500)
+
+        main_path = "/mnt/home/yajjour/task-specific-argument-mining-and-generation-data/argbench-dataset"
+        argbench_dataset = f"{main_path}/argbench-in-task"
+        dataset = load_from_disk(argbench_dataset)
+        self.assertEquals(dataset["test_counter_argument_generation_cmv_hua18"].shape[0], 500)
+
 
     def test_sampling_same(self):
         main_path = "/bigwork/nhwpajjy/task-specific-argument-mining-and-generation-data/argbench-dataset"
--- argbench/jobs/prompting/prompting_all_tasks.sh
+++ argbench/jobs/prompting/prompting_all_tasks.sh
@@ -42,10 +42,10 @@ else
 fi
 
 for model in "${models[@]}"; do
-#    bash argbench/jobs/prompting/prompting.sh "$gpu_count" "$gpu_type" prompting "${model:0:6}-0-shot"  --model "$model" --sample --seed 1517
-#    sleep 5
-#    bash argbench/jobs/prompting/prompting.sh "$gpu_count" "$gpu_type" prompting "${model:0:6}-4-shot"  --model "$model" -k 4 --sample --seed 1517
-#    sleep 5
+    bash argbench/jobs/prompting/prompting.sh "$gpu_count" "$gpu_type" prompting "${model:0:6}-0-shot"  --model "$model" --sample --seed 1517
+    sleep 5
+    bash argbench/jobs/prompting/prompting.sh "$gpu_count" "$gpu_type" prompting "${model:0:6}-4-shot"  --model "$model" -k 4 --sample --seed 1517
+    sleep 5
     bash argbench/jobs/prompting/prompting.sh "$gpu_count" "$gpu_type" prompting "${model:0:6}-cot" --model "$model" --cot--sample --seed 1517
     sleep 5
 done
