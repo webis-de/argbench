3f34f8756631ffd11d08a13eb16bf598e0acb162
Yamen <y.ajjour@ai.uni-hannover.de>
2025-09-19 14:45:15 +0200

    updating README.md

--- argbench/jobs/prompting/prompting_all_tasks.sh
+++ argbench/jobs/prompting/prompting_all_tasks.sh
@@ -46,6 +46,6 @@ for model in "${models[@]}"; do
     sleep 5
     bash argbench/jobs/prompting/prompting.sh "$gpu_count" "$gpu_type" prompting "${model:0:6}-4-shot"  --model "$model" -k 4 --sample --seed 1517
     sleep 5
-    bash argbench/jobs/prompting/prompting.sh "$gpu_count" "$gpu_type" prompting "${model:0:6}-cot" --model "$model" --cot--sample --seed 1517
+    bash argbench/jobs/prompting/prompting.sh "$gpu_count" "$gpu_type" prompting "${model:0:6}-cot" --model "$model" --cot --sample --seed 1517
     sleep 5
 done
