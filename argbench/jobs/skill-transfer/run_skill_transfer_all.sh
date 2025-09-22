332fd5066e407ae72a93a2286f7a4ef9bf84a631
Yamen <y.ajjour@ai.uni-hannover.de>
2025-09-22 16:28:37 +0200

    trying small batch

--- argbench/jobs/skill-transfer/run_skill_transfer_all.sh
+++ argbench/jobs/skill-transfer/run_skill_transfer_all.sh
@@ -60,9 +60,9 @@ for model in "${models[@]}"; do
       echo "test"
       if [ -n "$quantization" ] ; then
 
-          bash argbench/jobs/skill-transfer/run_skill_transfer_experiments.sh "$gpu_count" "$gpu_type" "${task}" "skill-transfer-$model-$task" --model "$model" --skill_filter "$task"  --quantization --train_epochs 1 --debug --sample
+          bash argbench/jobs/skill-transfer/run_skill_transfer_experiments.sh "$gpu_count" "$gpu_type" "${task}" "skill-transfer-$model-$task" --model "$model" --skill_filter "$task"  --quantization --train_epochs 1 --debug --sample --debug
         else
-          bash argbench/jobs/skill-transfer/run_skill_transfer_experiments.sh "$gpu_count" "$gpu_type" "${task}" "skill-transfer-$model-$task" --model "$model" --skill_filter "$task" --optim "adamw_torch" --train_epochs 1 --debug --sample
+          bash argbench/jobs/skill-transfer/run_skill_transfer_experiments.sh "$gpu_count" "$gpu_type" "${task}" "skill-transfer-$model-$task" --model "$model" --skill_filter "$task" --optim "adamw_torch" --train_epochs 1 --debug --sample --debug
         fi
 
     else
