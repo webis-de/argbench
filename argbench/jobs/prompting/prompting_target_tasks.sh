3f1b8d80d0dc0937b4a3ad9dc9799384d52318c2
Yamen <y.ajjour@ai.uni-hannover.de>
2025-09-23 14:57:00 +0200

    trying small batch

--- argbench/jobs/prompting/prompting_target_tasks.sh
+++ argbench/jobs/prompting/prompting_target_tasks.sh
@@ -65,13 +65,13 @@ fi
 for model in "${models[@]}"; do
   for task in "${tasks[@]}"; do
 
-##    bash argbench/jobs/prompting/prompting.sh "$gpu_count" "$gpu_type" 24:00:00 prompting "${task:0:6}-${model:0:6}-0-shot" --dataset "$task" --model "$model"  --sample
+##    bash argbench/jobs/prompting/prompting.sh "$gpu_count" "$gpu_type" 24:00:00 prompting "${task:0:6}-${model:0:6}-0-shot" --dataset "$task" --model "$model"  --sample "${args[@]}"
     sleep 5
-#    bash argbench/jobs/prompting/prompting.sh "$gpu_count" "$gpu_type" 24:00:00 prompting "${task:0:6}-${model:0:6}-1-shot" --dataset "$task" --model "$model" -k 1  --sample
+#    bash argbench/jobs/prompting/prompting.sh "$gpu_count" "$gpu_type" 24:00:00 prompting "${task:0:6}-${model:0:6}-1-shot" --dataset "$task" --model "$model" -k 1  --sample "${args[@]}"
 #    sleep 5
-#    bash argbench/jobs/prompting/prompting.sh "$gpu_count" "$gpu_type" 24:00:00 prompting "${task:0:6}-${model:0:6}-4-shot" --dataset "$task" --model "$model" -k 4 --sample
+#    bash argbench/jobs/prompting/prompting.sh "$gpu_count" "$gpu_type" 24:00:00 prompting "${task:0:6}-${model:0:6}-4-shot" --dataset "$task" --model "$model" -k 4 --sample "${args[@]}"
 #    sleep 5
-    bash argbench/jobs/prompting/prompting.sh "$gpu_count" "$gpu_type" 24:00:00 prompting "${task}-${model}-cot" --dataset "$task" --model "$model" --cot --sample
+    bash argbench/jobs/prompting/prompting.sh "$gpu_count" "$gpu_type" 24:00:00 prompting "${task}-${model}-cot" --dataset "$task" --model "$model" --cot --sample "${args[@]}"
 #    sleep 5
   done
 
