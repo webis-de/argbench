00f9108c38d945dc605da280fe1fc633a0f1a900
Yamen <y.ajjour@ai.uni-hannover.de>
2025-09-24 20:56:59 +0200

    trying small batch

--- argbench/experiment/run.py
+++ argbench/experiment/run.py
@@ -540,12 +540,14 @@ class Runner:
         self.trainer.train()
         log_mem(f"trained model")
 
-        self.free_model()
 
 
         sampling_params = self.load_sampling_params(self.test_dataset_name, trial, self.config.hpo_config.vllm_config)
         self.trainer.evaluate()
+
         metrics = self.evaluate(self.test_dataset_name, self.iterable_dataset["test"], sampling_params, model=self.trainer.model)
+        self.free_model()
+
         log_mem(f"finished evaluation")
         logger.debug(f"metrics are {metrics}")
         metric = self.task_metrics[self.test_dataset_name]
